15bcaecb0fb6c01b9a7b9dca55076045e97f2341
beatricekallen <bkallen90@gmail.com>
2022-07-09 20:20:47 -0400

    sets up files

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,30 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
