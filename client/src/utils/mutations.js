d9b401c6c6037d3f49e5ee17e6e7ffad158d7cc9
Alex Glaubitz <e.pirazzi@gmail.com>
2022-07-13 18:47:43 -0400

    front end add friend, add trip, delete trip, update trip mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,3 +23,55 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const ADD_FRIEND = gql`
+  mutation addFriend($friendId: ID!) {
+    addFriend(friendId: $friendId) {
+      _id
+      username
+      friendCount
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_TRIP = gql`
+  mutation addTrip($name: String!, $location: String, $dates: String, $transportation: String, $budget: String, $members: [String]) {
+    addTrip(name: $name, location: $location, dates: $dates, transportation: $transportation, budget: $budget, members: $members) {
+      creator
+      _id
+      name
+      location
+      dates
+      transportation
+      budget
+      createdAt
+      members
+    }
+  }
+`;
+
+export const DELETE_TRIP = gql`
+  mutation deleteTrip($_id: ID!) {
+    deleteTrip(_id: $_id) {
+      _id
+    }
+  }
+`;
+
+export const UPDATE_TRIP = gql`
+  mutation updateTrip($id: ID!, $location: String, $dates: String, $transportation: String, $budget: String, $members: [String]) {
+    updateTrip(_id: $id, location: $location, dates: $dates, transportation: $transportation, budget: $budget, members: $members) {
+      _id
+      location
+      dates
+      transportation
+      budget
+      members
+      name
+    }
+  }
+`;
