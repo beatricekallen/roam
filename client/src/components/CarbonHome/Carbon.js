e9d57592afd850b07c07305808f1b56cc0ccde67
beatricekallen <bkallen90@gmail.com>
2022-07-14 22:51:02 -0400

    sets up styling for carbon home component

--- client/src/components/CarbonHome/Carbon.js
+++ client/src/components/CarbonHome/Carbon.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./Carbon.css";
 
-export default function Carbon() {
+function Carbon() {
   return (
-    <div className="home__hero-section">
-      <div>
-        <div className="hero__content">
-          <h1>Roam</h1>
-          <p>contentoadflkjadfaldflnkadf</p>
-          <img className="hero__img" src="/" alt=""></img>
+    <div className="home__carbon-section carbon__img">
+      <div className="home__carbon-text-wrapper">
+        <div className="carbon__content">
+          <h2>Carbon Offsetting</h2>
+          <p>
+            Love traveling the world but want to do so sustainably? Offset the
+            carbon emissions of your trip by donating to a conservation charity
+            through our app.{" "}
+          </p>
+          <img className="carbon__img" src="/" alt=""></img>
         </div>
       </div>
     </div>
-  )
+  );
 }
+
+export default Carbon;
