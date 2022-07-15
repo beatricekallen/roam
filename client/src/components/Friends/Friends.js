955fe8e436537ed585db20b7b55d46ccd7d0fc07
beatricekallen <bkallen90@gmail.com>
2022-07-14 23:06:42 -0400

    sets up styling for friends home component

--- client/src/components/Friends/Friends.js
+++ client/src/components/Friends/Friends.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function Friends() {
   return (
-    <div className="home__hero-section">
-      <div>
-        <div className="hero__content">
-          <h1>Roam</h1>
-          <p>contentoadflkjadfaldflnkadf</p>
-          <img className="hero__img" src="/" alt=""></img>
+    <div className="home__friends-section friends__img">
+      <div className="home__friends-text-wrapper">
+        <div className="friends__content">
+          <h2>Easily Settle Expenses</h2>
+          <p>
+            Traveling with friends? Awesome. Trying to settle up expenses at the
+            end of a trip? Not so awesome. Our built-in functionality allows you
+            to track expenses and split them at the end of your trip.
+          </p>
+          <img className="friends__img" src="/" alt=""></img>
         </div>
       </div>
     </div>
-  )
+  );
 }
