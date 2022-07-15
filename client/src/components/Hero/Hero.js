b2803ae0ab6a99dfa16486977ec4d3e914f9590d
brittanymy <brittany.young2001@gmail.com>
2022-07-14 22:06:21 -0400

    updated hero styles

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -1,19 +1,26 @@
 import React from "react";
-import "./Hero.js";
+import { Link } from 'react-router-dom';
+import "./Hero.css";
 
 const Hero = () => {
   return (
-    <div className="home__hero-section">
+    <div className="home__hero-section hero__img">
       <div>
-        <div className="hero__content">
-          <h3>fljads</h3>
-          <h1>Roam</h1>
-          <p>contentoadflkjadfaldflnkadf</p>
-          <img className="hero__img" src="/" alt=""></img>
+        <div className="home__hero-text-wrapper">
+          <h3 className="top-line">fljads</h3>
+          <h1 className="heading">Roam</h1>
+          <p className="home__hero-subtitle">contentoadflkjadfaldflnkadf</p>
+          <div className="home__hero-img-wrapper">
+            <img className="home__hero-img" src="/" alt=""></img>
+          </div>
+          {/* <Link to="/">
+            <button>Discover More</button>
+          </Link> */}
+          <button>Discover More</button>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Hero;
