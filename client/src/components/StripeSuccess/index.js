61d0fbd0d6cf402b6e27f1211d9421f7b12c2bd9
beatricekallen <bkallen90@gmail.com>
2022-07-15 17:17:10 -0400

    styles stripe success component

--- client/src/components/StripeSuccess/index.js
+++ client/src/components/StripeSuccess/index.js
@@ -1,16 +1,24 @@
 //TODO: add functionality to button? or delete button if header is sufficient
 
-import Button from "@mui/material/Button";
+// import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+import "./index.css";
 
 const StripeSuccess = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h2>Success!</h2>
-      <p>
-        Thanks for your donation. Travelling sustainably and mindfully is good
-        for you and for the planet.
-      </p>
-      <Button size="small">Return To Your Profile</Button>
+    <div className="container">
+      <div className="stripe-success">
+        <h1>Success!</h1>
+        <h2>
+          Thanks for your donation. Travelling sustainably and mindfully is good
+          for you and for the planet.
+        </h2>
+        <button size="small" onClick={() => navigate("/profile")}>
+          Return To Your Profile
+        </button>
+      </div>
     </div>
   );
 };
