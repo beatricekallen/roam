9d56aa466aa6e29c4a6886e8bebe5da4c5d4da9d
beatricekallen <bkallen90@gmail.com>
2022-07-15 00:01:24 -0400

    sets up styling for footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,22 +1,25 @@
 import React from "react";
-import ItemsContainer from "./ItemsContainer";
-import SocialIcons from "./SocialIcons";
-import { Icons } from "./Menus";
+// import Box from "@mui/material/Box";
+// import ItemsContainer from "./ItemsContainer";
+// import SocialIcons from "./SocialIcons";
+// import { Icons } from "./Menus";
+// import BottomNavigation from "@mui/material/BottomNavigation";
+// import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 
-import './Footer.css';
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white">
-      <ItemsContainer />
+    <footer className="footer">
+      {/* <ItemsContainer />
       <div
         className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10
       text-center pt-2 text-gray-400 text-sm pb-8"
-      >
-        <span>© 2022 Roam. All rights reserved.</span>
-        <span>Terms · Privacy Policy</span>
-        <SocialIcons Icons={Icons} />
-      </div>
+      > */}
+      <span>© 2022 Roam. All rights reserved.</span>
+      <span>Terms · Privacy Policy</span>
+      {/* <SocialIcons Icons={Icons} /> */}
+      {/* </div> */}
     </footer>
   );
 };
