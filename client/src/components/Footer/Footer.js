0fbe3730c946a518e9f332b28bcebf9a5b2d2765
brittanymy <brittany.young2001@gmail.com>
2022-07-16 19:23:03 -0400

    navigation update

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -10,26 +10,44 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer>
-        <div className="footer-container">
-            <div className="footer-box">
-                <div className="footer-navigation">
-                    <Link to="/" className="footer-link">Trips</Link>
-                    <Link to="/" className="footer-link">Splitwise</Link>
-                    <Link to="/" className="footer-link">Carbon</Link>
-                    <Link to="/" className="footer-link">Login</Link>
-                </div>
-            </div>
-            <div className="footer-icon__container">
-                <Link to={{ pathname: "https://twitter.com/?lang=en"}} target="_blank" rel="noopener noreferrer" className="footer-icon">{TwitterIcon}</Link>
-                <Link to={{ pathname: "https://www.facebook.com/"}} target="_blank" rel="noopener noreferrer" className="footer-icon">{FacebookIcon}</Link>
-                <Link to={{ pathname: "https://www.instagram.com/"}} target="_blank" rel="noopener noreferrer" className="footer-icon">{InstagramIcon}</Link>
-                <Link to={{ pathname: "https://github.com/beatricekallen/project-three"}} target="_blank" rel="noopener noreferrer" className="footer-icon">{GitHubIcon}</Link>
-            </div>
-            <div className="copyright">
-                <p className="copy">© 2022 Roam. All rights reserved.</p>
-            </div>
+    <div className="footer-container">
+      <div className="footer-box">
+        <div className="footer-navigation">
+          <Link to="/" className="footer-link">
+            Trips
+          </Link>
+          <Link to="/" className="footer-link">
+            Splitwise
+          </Link>
+          <Link to="/" className="footer-link">
+            Carbon
+          </Link>
+          <Link to="/" className="footer-link">
+            Login
+          </Link>
         </div>
-    </footer>
+      </div>
+      <div className="footer-icon__container">
+        <Link to={"https://twitter.com/?lang=en"} target="_blank" rel="noopener" className="footer-icon">
+          <TwitterIcon />
+        </Link>
+        <Link to={"https://www.facebook.com/"} target="_blank" rel="noopener" className="footer-icon">
+          <FacebookIcon />
+        </Link>
+        <Link to={"https://www.instagram.com/"} target="_blank" rel="noopener" className="footer-icon">
+          <InstagramIcon />
+        </Link>
+        <Link to={"https://github.com/beatricekallen/project-three"}
+          target="_blank" rel="noopener" className="footer-icon"
+        >
+          <GitHubIcon />
+        </Link>
+      </div>
+      <div className="copyright">
+        <p className="copy">© 2022 Roam. All rights reserved.</p>
+      </div>
+    </div>
+  </footer>
   )
 };
 
