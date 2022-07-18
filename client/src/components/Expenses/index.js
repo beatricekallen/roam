5fc8d9ec033801415e9a2bd2e60b381153bfc27e
llattanzi <lauren71442@gmail.com>
2022-07-18 16:43:44 -0400

    update expenses

--- client/src/components/Expenses/index.js
+++ client/src/components/Expenses/index.js
@@ -1,11 +1,13 @@
 import { QUERY_TRIP_EXPENSES } from "../../utils/queries";
+import { useQuery, useMutation } from '@apollo/client';
 import "./index.css";
 
 const Expenses = ({trip}) => {
     const {loading, data: expenses} = useQuery(trip && QUERY_TRIP_EXPENSES, {
         variables: {id: trip._id}
-        
-});
+    });
+
+
 
 
 }
